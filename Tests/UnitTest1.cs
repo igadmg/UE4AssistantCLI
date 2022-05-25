dbf9516eb7a7b524f78cb27ac123e9ef537fedc8
Igor Karatayev <igor.cwer@gmail.com>
2022-05-25 21:23:56 +0300

    tests

---
+++ Tests/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using SystemEx;
+using UE4Assistant;
+using UE4AssistantCLI.UI;
+
+namespace Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+        var str = "UFUNCTION(Category = \"sdfsd\", BlueprintPure, meta=(DefaultToSelf = \"Object\"))";
+
+        if (Specifier.TryParse(str.tokenize(), out var specifier))
+        {
+            var so = new SpeсifierTypeDescriptor(specifier);
+        }
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
