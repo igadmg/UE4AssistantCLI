054ea8cefe0ac48254c52f2e638e327e5ed641bd
Igor Karatayev <igor.cwer@gmail.com>
2022-06-14 16:06:45 +0300

    simple CLI interface.

---
+++ UE4AssistantCLI.UI/CLI.cs
@@ -0,0 +1,13 @@
+﻿namespace UE4AssistantCLI.UI;
+
+using ConsoleAppFramework;
+
+internal class CLI : ConsoleAppBase
+{
+	[Command("parse")]
+	public async void Parse([Option("ci")] int caret_index = 0)
+	{
+		var result = await Program.ParseAsync(Console.ReadLine(), caret_index);
+		Console.WriteLine(result);
+	}
+}
--- UE4AssistantCLI.UI/Program.cs
+++ UE4AssistantCLI.UI/Program.cs
@@ -1,7 +1,32 @@
 namespace UE4AssistantCLI.UI;
 
+using ConsoleAppFramework;
+using System.Runtime.InteropServices;
+using SystemEx;
+using UE4Assistant;
+
 static class Program
 {
+	private static Specifier specifier;
+	private static Form mainForm;
+
+	[DllImport("kernel32.dll", SetLastError = true)]
+	[return: MarshalAs(UnmanagedType.Bool)]
+	static extern bool AllocConsole();
+
+
+
+	internal static async Task<string> ParseAsync(string str, int caret_index)
+	{
+		var result = str;
+
+		var signal = new SemaphoreSlim(0, 1);
+		Application.Run(MacroForm(str, caret_index, str => { result = str; signal.Release(); }));
+		await signal.WaitAsync();
+
+		return result;
+	}
+
 	/// <summary>
 	///  The main entry point for the application.
 	/// </summary>
@@ -11,6 +36,47 @@ static class Program
 		// To customize application configuration such as set high DPI settings or default font,
 		// see https://aka.ms/applicationconfiguration.
 		ApplicationConfiguration.Initialize();
-		Application.Run(new FormMacroEditor("UFUNCTION(Category = \"sdfsd\", BlueprintPure, meta=(DefaultToSelf = \"Object\"))"));
+		AllocConsole();
+
+		Console.SetIn(new StringReader("	UPROPERTY(Category = \"Default\", VisibleAnywhere, BlueprintReadOnly) int i = 0;\n"));
+
+		ConsoleApp.CreateBuilder(Environment.GetCommandLineArgs().Skip(1)
+				, options => {
+					options.HelpSortCommandsByFullName = true;
+				})
+			.Build()
+			.AddCommands<CLI>()
+			.Run();
 	}
+
+	public static FormMacroEditor MacroForm(string str, int caret_index, Action<string> a)
+		=> Specifier.FindAll(str, caret_index).First()
+			.Let(specifier => new FormMacroEditor(specifier.s)
+				.Also(_ => {
+					_.FormClosed += (object? sender, FormClosedEventArgs e) => {
+						var form = (FormMacroEditor)sender;
+						if (form.DialogResult == DialogResult.OK)
+						{
+							str = str
+								.Remove(specifier.si, specifier.ei - specifier.si)
+								.Insert(specifier.si, form.specifier.ToString());
+						}
+
+						a(str);
+					};
+				}));
+}
+
+public static class StreamEx
+{
+	public static Stream Write(this Stream stream, string str)
+		=> stream.Also(_ => {
+			var data = str.ToByte();
+			stream.Write(data, 0, data.Length);
+		});
+	public static Task WriteAsync(this Stream stream, string str)
+		=> stream.Let(_ => {
+			var data = str.ToByte();
+			return stream.WriteAsync(data, 0, data.Length);
+		});
 }
